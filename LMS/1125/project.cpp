451e0ac1b82d2744b3af3229eae72f68ac70bba2
minseok0123 <21.minseok.lee@gmail.com>
2021-11-25 09:45:44 +0900

    11월 25일 이름순 정렬

---
+++ LMS/1125/project.cpp
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include <string.h>
+char value;
+
+
+void put()
+{
+	printf("\n물건을 채우는 공간입니다.\n");
+	char name[3][10];
+    char tname[10];
+    int i,j;
+    int scores[3][3];
+    int tscores;
+    for(i=0;i<3;i++){// 이름 및 점수 입력
+
+        printf("상품명 : ");
+
+        scanf("%s",name[i]);
+
+        
+
+        printf("가격 입력 : 예) 1000\n");
+
+        scanf("%d", &scores[i][0],&scores[i][1],&scores[i][2]);
+
+    }
+    for(i=0;i<3;i++)//이름순으로 정렬하기
+    {
+        for(j=i;j<3;j++)
+        {
+            if(strcmp(name[i],name[j]) > 0 )
+            {
+                strcpy(tname,name[j]);
+                strcpy(name[j],name[i]);
+                strcpy(name[i],tname);
+                tscores=scores[j][0];
+                scores[j][0]=scores[i][0];
+                scores[i][0]=tscores;
+                tscores=scores[j][1];
+                scores[j][1]=scores[i][1];
+                scores[i][1]=tscores;
+                tscores=scores[j][2];
+                scores[j][2]=scores[i][2];
+                scores[i][2]=tscores;
+            }
+        }
+    }
+    printf("\n----------창고에 채운 목록-----------\n");
+    printf("-------------------------------------\n");
+    for(i=0;i<3;i++)
+    {
+        printf("%s %d\n", name[i],scores[i][0]);
+    }
+}
+void buy()
+{
+	printf("\n물건을 구매하는 공간입니다."); 
+}
+
+
+int main()
+{
+	printf("***소마할일마트에 오신것을 환영합니다.***\n\n");
+	printf("물건을 채우시려면 F를 눌러주세요\n");
+	printf("상품을 구매하시려면 B를 눌러주세요\n\n");
+	printf("입력 : -> ");
+	scanf("%c",&value);
+	if(value=='F')
+	{
+		put();
+	}
+	else if(value=='B')
+	{
+		buy();
+	}
+	 
+}
